3176669421fd5a5c327eb741b877d61925171d16
johnny-gill <uint777@gmail.com>
2023-04-07 14:51:31 +0900

    입력값에 대한 validation. 숫자변환은 +로 가능

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import Button from "../UI/Button";
-import Card from "../UI/Card";
-import classes from "./AddUser.module.css";
+import { useState } from 'react';
+import Button from '../UI/Button';
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
@@ -10,7 +10,18 @@ const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
 
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+
+    if (+enteredAge < 1) { // 변수명앞에 +붙여주면 숫자로 바뀜
+      return;
+    }
+
     console.log(enteredUsername, enteredAge);
+
+    setEnteredUsername('');
+    setEnteredAge('');
   };
 
   const usernameChangeHandler = (event) => {
@@ -25,9 +36,19 @@ const AddUser = (props) => {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">Age</label>
-        <input id="age" type="number" onChange={ageChangeHandler} />
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
