a3ab99fa2b2339fd08525a4602184ed7bddefeaa
johnny-gill <uint777@gmail.com>
2023-04-07 11:07:02 +0900

    유저추가 컴포넌트 생성

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,19 @@
-const AddUser = props => {
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    
+    debugger;
+  };
 
+  return (
+    <form onSubmit={addUserHandler}>
+      <label htmlFor="username">Username</label>
+      <input id="username" type="text" />
+      <label htmlFor="age">Age</label>
+      <input id="age" type="number" />
+      <button type="submit">유저 추가</button>
+    </form>
+  );
 };
 
 export default AddUser;
